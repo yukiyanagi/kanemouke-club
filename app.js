8deed624b3be386d2777bdf131835f182eb54bd3
y.yanagi <y.yanagi@leihauoli.com>
2016-12-26 18:30:03 +0900

    指摘箇所修正とsmartphone-appページ追加

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var smartphoneApps = require('./routes/smartphone-app');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/smartphone_app', smartphoneApps);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
